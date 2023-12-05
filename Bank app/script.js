373c3f75c54c79d87537d00733b5a128565d21d7
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-12-05 20:29:49 +0100

    Update script.js

--- Bank app/script.js
+++ Bank app/script.js
@@ -74,3 +74,17 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(function (name) {
+        return name[0];
+      })
+      .join('');
+  });
+};
+
+createUsernames(accounts);
